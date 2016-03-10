b9b403efe613e4233a4404262f2241e608f3b370
Mats Jensen <matsjensen@Matss-MacBook-Pro.local>
2016-03-10 19:10:12 +0100

    its working

--- todo/app/scripts/controllers/main.js
+++ todo/app/scripts/controllers/main.js
@@ -7,11 +7,29 @@
  * # MainCtrl
  * Controller of the todoApp
  */
+
 angular.module('todoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('MainCtrl', function ($scope, localStorageService) {
+
+  //stores added todos
+  var todosInStore = localStorageService.get('todos');
+
+  $scope.todos = todosInStore || [];
+
+  //watch for changes in $scope.todos
+  $scope.$watch('todos', function () {
+    localStorageService.set('todos', $scope.todos);
+  }, true);
+
+  //add task
+  $scope.addTodo = function () {
+    $scope.todos.push($scope.todo);
+    $scope.todo = '';
+  };
+
+  //remove task
+  $scope.removeTodo = function (index) {
+    $scope.todos.splice(index, 1);
+  };
+
+});
